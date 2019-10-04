b585fda17080dec964ec681988ec90d09b8dabfc
Joe Vargas <joeavargas@outlook.com>
2019-10-04 09:11:19 -0500

    stopped tracking xcuserstate file

---
+++ MyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProject
+//
+//  Created by Joe Vargas on 10/4/19.
+//  Copyright © 2019 Joe Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
